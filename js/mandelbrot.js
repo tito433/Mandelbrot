f8746c5af1a0b22d70e23d66a0caee8909eba278
tito <tito@cefalo.com>
2016-10-13 10:34:35 +0600

    Mouse drag added.

--- js/mandelbrot.js
+++ js/mandelbrot.js
@@ -1,3 +1,75 @@
+var Mouse=function(canvas){
+	if (!(this instanceof Mouse)){
+         return new Mouse(canvas);
+     }
+    if(undefined === canvas){
+    	throw "Undefined canvas!!!! Nothing for mouse...";
+    }
+
+
+    this.bounds=canvas.getBoundingClientRect();
+    this._isDown=false;
+    this.prevCords={x:0,y:0};
+    this._evt=[];
+
+
+    this._onClickDown=function(e){
+    	this._isDown=true;
+    	this.prevCords=this._getMousePos(e);
+    }
+    this._onMove=function(e){
+    	if(this._isDown && this._evt['drag']){
+    		var cp=this._getMousePos(e);
+    		var delta={x:this.prevCords.x-cp.x,y:this.prevCords.y-cp.y};
+    		this.prevCords=cp;
+    		this._callEvt("drag",delta);
+
+    	}
+    }
+    this._onClickUp=function(e){
+    	this._isDown=false;
+    }
+    this._onCancel=function(e){
+    	this._isDown=false;
+    }
+
+    this._callEvt=function(key,e){
+    	if(this._evt && this._evt[key]){
+    		this._evt[key].forEach(function(elem,idx){
+    			elem(e);
+    		});
+    	}
+    }
+    this.on=function(evtName,callBack){
+    	if(this._evt.length>0){
+    		if(this._evt[evtName]){
+    			this._evt[evtName].push(callBack);
+    			return true;
+    		}
+    	}
+    	
+    	this._evt[evtName]=[callBack];
+    	return true;
+    }
+    this._getMousePos=function(evt) {
+        return {
+          x: evt.clientX - this.bounds.left,
+          y: evt.clientY - this.bounds.top
+        };
+      }
+
+    //click down
+    canvas.addEventListener("mousedown", this._onClickDown.bind(this), false);
+    canvas.addEventListener("touchstart", this._onClickDown.bind(this), false);
+    //move
+    canvas.addEventListener("mousemove", this._onMove.bind(this), false);
+    canvas.addEventListener("touchmove", this._onMove.bind(this), false);
+    //click up
+    canvas.addEventListener("mouseup", this._onClickUp.bind(this), false);
+    canvas.addEventListener("touchend", this._onClickUp.bind(this), false);
+    canvas.addEventListener("touchcancel", this._onCancel.bind(this), false);
+}
+
 var Mandelbrot=function(canvas){
 	if (!(this instanceof Mandelbrot)){
          return new Mandelbrot(canvas);
@@ -5,11 +77,16 @@ var Mandelbrot=function(canvas){
     if(undefined === canvas){
     	throw "Undefined canvas!!!! Nothing to show...";
     }
+
 	this.size = canvas.width;
 	this.ctx= canvas.getContext('2d');;
 	this.image =this.ctx.createImageData(this.size,this.size);
-
+	/* If you write any private function like this:
+			var draw=function(){}
+		then it will have a global scope of window. i.e. this will reffer to window
+	*/
 	this.setup=function(){
+		this.zoom=1;
 		this.view=[-2,2,-2,2];
 		this.draw();
 	}
@@ -49,6 +126,21 @@ var Mandelbrot=function(canvas){
 	    return  (x-a)/(b-a) * (d-c) + c;
 	}
 
+	this.onDrag=function(delta){
+		var px=(delta.x/this.size ) * (Math.abs(this.view[0])+Math.abs(this.view[1])),
+			py=(delta.y/this.size ) * (Math.abs(this.view[2])+Math.abs(this.view[3]));
+		
+		this.view[0]+=px;
+		this.view[1]+=px;
+		this.view[2]+=py;
+		this.view[3]+=py;
+		this.draw();
+	}
+
+	this.mouse=new Mouse(canvas);
+
 	this.setup();
+	//set drag event.
+	this.mouse.on('drag',this.onDrag.bind(this));
 	
 }
