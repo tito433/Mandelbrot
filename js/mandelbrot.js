95bc431ce2ce2cd2a390d0a86e6caffe67b09390
tito <tito@cefalo.com>
2016-10-13 09:10:27 +0600

    code refactored.

--- js/mandelbrot.js
+++ js/mandelbrot.js
@@ -1,50 +1,54 @@
-var size = 600;//window.innerHeight || document.documentElement.clientHeight	|| document.body.clientHeight;
-var ctx=false;
-var canvasImage =false;
-var pixels=[];
+var Mandelbrot=function(canvas){
+	if (!(this instanceof Mandelbrot)){
+         return new Mandelbrot(canvas);
+     }
+    if(undefined === canvas){
+    	throw "Undefined canvas!!!! Nothing to show...";
+    }
+	this.size = canvas.width;
+	this.ctx= canvas.getContext('2d');;
+	this.image =this.ctx.createImageData(this.size,this.size);
 
-function setup(){
-	var canvas = document.getElementById('canvas');
-    canvas.width = canvas.height=size;
-    ctx = canvas.getContext('2d');
-    canvasImage = ctx.createImageData(size,size);
-    pixels=canvasImage.data;
-}
-
-function draw(){
-
-	for(var x=0;x<size;x++){
-		for(var y=0;y<size;y++){
-
-			var a=map(x,0,size,-2,2);
-			var b=map(y,0,size,-2,2);
-			var ca=a;
-			var cb=b;
-			var bright=255;
-
-			while(bright>0){
-				var aa=a*a-b*b,
-					bb=2*a*b;
-				a=aa+ca;
-				b=bb+cb;
-
-				if(Math.abs(a+b)>2) break;
-				
-				bright--;
-			}
-
-			var pix=(x+y*size) * 4;
-			pixels[pix+0]=bright;
-			pixels[pix+1]=bright;
-			pixels[pix+2]=bright;
-			pixels[pix+3]=255;
-		}
+	this.setup=function(){
+		this.view=[-2,2,-2,2];
+		this.draw();
 	}
-	ctx.putImageData(canvasImage, 0, 0);
-}
-function map(x, a, b, c, d) {
-    return  (x-a)/(b-a) * (d-c) + c;
-}
 
-setup();
-draw();
+	this.draw=function(){
+
+		for(var x=0;x<this.size;x++){
+			for(var y=0;y<this.size;y++){
+
+				var a=this.map(x,0,this.size,this.view[0],this.view[1]);
+				var b=this.map(y,0,this.size,this.view[2],this.view[3]);
+				var ca=a;
+				var cb=b;
+				var bright=255;
+
+				while(bright>0){
+					var aa=a*a-b*b,
+						bb=2*a*b;
+					a=aa+ca;
+					b=bb+cb;
+
+					if(Math.abs(a+b)>2) break;
+					
+					bright--;
+				}
+
+				var pix=(x+y*this.size) * 4;
+				this.image.data[pix+0]=bright;
+				this.image.data[pix+1]=bright;
+				this.image.data[pix+2]=bright;
+				this.image.data[pix+3]=255;
+			}
+		}
+		this.ctx.putImageData(this.image, 0, 0);
+	}
+	this.map=function(x, a, b, c, d) {
+	    return  (x-a)/(b-a) * (d-c) + c;
+	}
+
+	this.setup();
+	
+}
